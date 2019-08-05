0307db04c023012e08cbbcaf86a6418f46360c78
Nineth! <41068598+ninethbacovic@users.noreply.github.com>
2019-08-05 20:52:42 -0300

    Website changed

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+#Importamos a função index() definida no arquivo views.py
+from . import views
+
+app_name = 'website'
+
+# urlpatterns contém a lista de roteamentos de URLs
+urlpatterns = [
+    
+]
